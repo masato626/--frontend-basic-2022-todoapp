719229992bb0ed34f2fd4939b3103ccfd26436c4
masato626 <m.takiguchi.22.7.561@4nonome.com>
2023-05-17 13:47:09 +0900

    a

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -1,13 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 
-const Input = ({ onEdotComplete, defaultValue = "" }) => {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    ref.current.value = defaultValue;
-  }, []);
-
+const Input = ({ onEdotComplete, defaultValue }) => {
   return <StyledInput ref={ref} />;
 };
 export default Input;

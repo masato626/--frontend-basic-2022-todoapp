8356508d55cdda910e1ed6939362303dfed6188d
masato626 <m.takiguchi.22.7.561@4nonome.com>
2023-06-19 20:46:53 +0900

    インポート

--- src/components/Organisms/Todocard/index.jsx
+++ src/components/Organisms/Todocard/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import AddTaskButton from "../../Atoms/addtaskbutton";
+import AddTaskButton from "../../Atoms/AddTaskButton";
 import Task from "../../Molecules/Task";
 
 const TodoCard = () => {

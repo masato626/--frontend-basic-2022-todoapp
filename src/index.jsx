bc8d8bc449a439f23d7d26d867896715954cb3a8
masato626 <m.takiguchi.22.7.561@4nonome.com>
2023-09-10 23:48:03 +0900

    pa

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import MainPage from "./components/Pages/Mainpage";
-import "./style.css";
-
-ReactDOM.render(<MainPage />, document.getElementById("app"));

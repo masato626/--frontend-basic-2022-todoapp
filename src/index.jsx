3836464e335475dee2b7285eb2cbcce2a227c1e8
masato626 <m.takiguchi.22.7.561@4nonome.com>
2023-09-21 08:31:58 +0900

    完成

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MainPage from "./components/Pages/Mainpage";
 import "./style.css";
-
 ReactDOM.render(<MainPage />, document.getElementById("app"));

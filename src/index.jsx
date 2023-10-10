aaaa91e44495960e8f64de551641f8cb18fad911
masato626 <m.takiguchi.22.7.561@4nonome.com>
2023-10-10 13:54:18 +0900

    パスを修正

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import MainPage from "./components/Pages/MainPage";
 import "./style.css";
 import AlertManager from "./components/Organisms/AlertManager";
-import { AlertHandlerProvider } from "../src/contexts/alert_handler";
+import { AlertHandlerProvider } from "./contexts/alert_handler";
 ReactDOM.render(
   <AlertHandlerProvider>
     <MainPage />

e2c6ccdaa0e0aa7d9b56dc5e8d0e247c049da857
masato626 <m.takiguchi.22.7.561@4nonome.com>
2023-10-15 17:59:09 +0900

    完成

--- src/contexts/alert_handler.jsx
+++ src/contexts/alert_handler.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState } from "react";
 
-const AlertHandlerContext = createContext();
+export const AlertHandlerContext = createContext();
 
 export const AlertHandlerProvider = ({ children }) => {
   const [alertState, setAlertState] = useState({
